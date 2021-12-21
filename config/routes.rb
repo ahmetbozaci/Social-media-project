85f218e40aac562e601f29b30e7be3ca17e31e8d
Bishoy <bishoy.samwuel@gmail.com>
2021-12-21 15:33:54 +0200

    Applying Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   get "getPosts", to: "posts#getPosts"
   get "getComments/:post_id", to: "comments#getComments"
   post "postComment/:user_id/:post_id/:content", to: "comments#post_comment"
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
