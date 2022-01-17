fe4a0dc9a0eb11c90f529c306d0bc55bbb5805d9
Ahmet Bozaci <ahmt9417@gmail.com>
2022-01-17 17:46:49 +0300

    Apply authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  skip_before_action :authorize_request, only: :create
+  protect_from_forgery with: :null_session, only: [:create]
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
   def index
     @users = User.all.where.not(email: current_user.email)
   end
